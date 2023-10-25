ad0c44a929595b946b1cfe325333d85a1f2b2bc7
lanoth <78845859+lanoth@users.noreply.github.com>
2023-10-25 15:21:31 +0200

    Added MovieSearchDropdownDto + MovieSearchResultDto

---
+++ DataLayer/DataTransferObjects/MovieSearchDropdownDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DataLayer.DataTransferObjects;
+
+public class MovieSearchDropdownDto
+{
+    public string Title { get; set; }
+    public int StartYear { get; set; }
+    public List<PersonDto> PersonDtos { get; set; }
+    public string Poster { get; set; }
+}
