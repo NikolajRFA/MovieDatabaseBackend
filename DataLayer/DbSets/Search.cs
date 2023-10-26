a6eb72245b4d3637eb331b8fd09cc1ad13ecba43
NikolajRFA <nrfadk@gmail.com>
2023-10-26 17:05:07 +0200

    Search.cs

---
+++ DataLayer/DbSets/Search.cs
@@ -0,0 +1,9 @@
+﻿namespace DataLayer.DbSets;
+
+public class Search
+{
+    public int Id { get; set; }
+    public string SearchPhrase { get; set; }
+    public DateTime Date { get; set; }
+    public User User { get; set; }
+}
