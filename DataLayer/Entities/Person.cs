b137eeec50dfdbdf5e2571a415973471a77d0407
lanoth <78845859+lanoth@users.noreply.github.com>
2023-10-27 08:59:39 +0200

    added automated entities

---
+++ DataLayer/Entities/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLayer.Entities;
+
+public partial class Person
+{
+    public string Nconst { get; set; } = null!;
+
+    public string? Personname { get; set; }
+
+    public string? Birthyear { get; set; }
+
+    public string? Deathyear { get; set; }
+
+    public decimal? NameRating { get; set; }
+
+    public virtual ICollection<Bookmark> Bookmarks { get; set; } = new List<Bookmark>();
+
+    public virtual ICollection<Crew> Crews { get; set; } = new List<Crew>();
+
+    public virtual ICollection<Profession> Professions { get; set; } = new List<Profession>();
+}
