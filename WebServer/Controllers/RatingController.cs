59b5b8bec370a049b2043e4217a4404c8c51596d
NikolajRFA <nrfadk@gmail.com>
2023-10-27 13:26:51 +0200

    Rename

---
+++ WebServer/Controllers/RatingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.Controllers
+{
+    public class RatingController
+    {
+        
+        /*public IActionResult Index()
+        {
+            return new Ok();
+        }*/
+    }
+}
