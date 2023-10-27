8f271cb1dc2b2ed854aa30fd461445407e341ab1
Haedric279 <Oscarcille@gmail.com>
2023-10-27 14:34:15 +0200

    Added Dataservice

---
+++ WebServer/DataTransferObjects/Profiles/RatingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DataLayer.DataTransferObjects;
+using DataLayer.DbSets;
+
+namespace WebServer.DataTransferObjects.Profiles;
+
+public class RatingProfile : Profile
+{
+    public RatingProfile()
+    {
+        CreateMap<Rating, RatingDto>();
+    }
+}
--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<BookmarkDataService>();
+builder.Services.AddSingleton<RatingDataService>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
