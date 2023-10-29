6ceac78b48b3cf2c22f548c4eb72337d05c4157e
Haedric279 <Oscarcille@gmail.com>
2023-10-29 11:49:52 +0100

    GetRatings finished

---
+++ WebServer/Models/RatingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServer.Models
+{
+    public class RatingModel
+    {
+       public int UserId { get; set; }
+       public string tconst { get; set; }
+       public double rating { get; set; }
+    
+    }
+}
