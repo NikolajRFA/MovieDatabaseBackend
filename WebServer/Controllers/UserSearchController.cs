7eed6697004303e1e2cacb3be5f27dfa078d2c43
lanoth <78845859+lanoth@users.noreply.github.com>
2023-11-08 15:54:25 +0100

    Added auth to searches

--- WebServer/Controllers/UserSearchController.cs
+++ WebServer/Controllers/UserSearchController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DataLayer.DataServices;
 using DataLayer.DbSets;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebServer.Controllers;
 using WebServer.DataTransferObjects;
@@ -10,74 +11,73 @@ namespace WebServer.Controllers;
 
 [Route("api/users/{userId:int}/searches")]
 [ApiController]
+[Authorize]
+public class UserSearchController : GenericControllerBase
+{
+    private readonly UserSearchDataService _dataService;
 
-
-    public class UserSearchController : GenericControllerBase
+    public UserSearchController(UserSearchDataService dataService, LinkGenerator linkGenerator, IMapper mapper) :
+        base(
+            linkGenerator, mapper)
     {
-        private readonly UserSearchDataService _dataService;
+        _dataService = dataService;
+    }
 
-        public UserSearchController(UserSearchDataService dataService, LinkGenerator linkGenerator, IMapper mapper) :
-            base(
-                linkGenerator, mapper)
+    [HttpDelete("{searchPhrase}")]
+    public IActionResult DeleteSearch(int userId, string searchPhrase)
+    {
+        if (userId != UserId) return Unauthorized();
+        _dataService.DeleteSearch(userId, searchPhrase);
+        return Ok("Search Deleted");
+    }
+
+    [HttpDelete("clear")]
+    public IActionResult DeleteSearches(int userId)
+    {
+        if (userId != UserId) return Unauthorized();
+        _dataService.DeleteSearches(userId);
+        return Ok("Searches Deleted");
+    }
+
+    [HttpGet(Name = nameof(GetSearches))]
+    public IActionResult GetSearches(int userId, int page = 0, int pageSize = 10)
+    {
+        if (userId != UserId) return Unauthorized();
+        var (searches, count) = _dataService.GetSearches(userId, page, pageSize);
+        List<SearchDto> searchDtos = new List<SearchDto>();
+        foreach (var search in searches)
         {
-            _dataService = dataService;
-
-        }
-
-        [HttpDelete("{searchPhrase}")]
-        public IActionResult DeleteSearch(int userId, string searchPhrase)
-        {
-            _dataService.DeleteSearch(userId, searchPhrase);
-            return Ok();
-        }
-
-        [HttpDelete("clear")]
-        public IActionResult DeleteSearches(int userId)
-        {
-            _dataService.DeleteSearches(userId);
-            return Ok();
-        }
-
-        [HttpGet(Name = nameof(GetSearches))]
-        public IActionResult GetSearches(int userId, int page = 0, int pageSize = 10)
-        {
-            var (searches, count) = _dataService.GetSearches(userId, page, pageSize);
-            List<SearchDto> searchDtos = new List<SearchDto>();
-            foreach (var search in searches)
-            {
-                var dto = MapSearch(search);
-                searchDtos.Add(dto);
-            } 
-        return Ok(Paging(searchDtos, count, new 
-                UserSearchPagingValues{ UserId = userId, Page = page, PageSize = pageSize }, nameof(GetSearches)));
-        }
-
-
-        [HttpGet("{searchPhrase}", Name=nameof(GetSearch))]
-        public ActionResult GetSearch(int userId, string searchPhrase)
-
-        {
-            var search = _dataService.GetSearch(userId, searchPhrase);
-            if (search == null) return NotFound();
             var dto = MapSearch(search);
-            return Ok(dto);
+            searchDtos.Add(dto);
         }
 
-        private SearchDto MapSearch(Search search)
-        {
-            var searchDto = Mapper.Map<SearchDto>(search);
-            searchDto.Url = GetUrl(nameof(GetSearch), new { userId = search.Id, search.SearchPhrase });
-            searchDto.User = GetUrl(nameof(UsersController.GetUser), new { search.Id });
-            searchDto.SearchPhrase = search.SearchPhrase;
-            searchDto.Date = search.Date;
-            return searchDto;
-        }
+        return Ok(Paging(searchDtos, count, new
+            UserSearchPagingValues { UserId = userId, Page = page, PageSize = pageSize }, nameof(GetSearches)));
+    }
 
-        private class UserSearchPagingValues : PagingValues
-        {
-            public int UserId { get; set; }
-        }
+
+    [HttpGet("{searchPhrase}", Name = nameof(GetSearch))]
+    public ActionResult GetSearch(int userId, string searchPhrase)
+    {
+        if (userId != UserId) return Unauthorized();
+        var search = _dataService.GetSearch(userId, searchPhrase);
+        if (search == null) return NotFound();
+        var dto = MapSearch(search);
+        return Ok(dto);
+    }
+
+    private SearchDto MapSearch(Search search)
+    {
+        var searchDto = Mapper.Map<SearchDto>(search);
+        searchDto.Url = GetUrl(nameof(GetSearch), new { userId = search.Id, search.SearchPhrase });
+        searchDto.User = GetUrl(nameof(UsersController.GetUser), new { search.Id });
+        searchDto.SearchPhrase = search.SearchPhrase;
+        searchDto.Date = search.Date;
+        return searchDto;
+    }
+
+    private class UserSearchPagingValues : PagingValues
+    {
+        public int UserId { get; set; }
+    }
 }
-
-
-
