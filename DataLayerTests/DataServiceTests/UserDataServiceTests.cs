76f69b18abc03625e27f814a6ec101d5ef3b979a
NikolajRFA <nrfadk@gmail.com>
2023-11-09 11:53:40 +0100

    Created more tests for user

--- DataLayerTests/DataServiceTests/UserDataServiceTests.cs
+++ DataLayerTests/DataServiceTests/UserDataServiceTests.cs
@@ -24,14 +24,14 @@ public class UserDataServiceTests
         {
             _testOutputHelper.WriteLine($"user: {user.Username} has {user.Searches.Count} searches.");
         });
-        Assert.NotNull(users);
+        Assert.NotNull(users.users);
     }
 
     [Fact]
     public void GetUser_UserId2_GetsNikoUser()
     {
         var dataService = new UserDataService();
-        var user = dataService.GetUser(2);
+        var user = dataService.GetUser(35);
         _testOutputHelper.WriteLine($"Username: {user.Username}");
         Assert.Equal("Niko", user.Username);
     }
@@ -42,5 +42,68 @@ public class UserDataServiceTests
         var dataService = new UserDataService();
         var user = dataService.GetUser(9999999);
         Assert.Null(user);
+    }
+
+    [Fact]
+    public void CreateUser_NewUser_ReturnsNewUser()
+    {
+        var dataService = new UserDataService();
+        var newUser = new
+        {
+            Username = "new_user",
+            Email = "new_user@email.com",
+            Password = "1234",
+            Salt = "12341234"
+        };
+        var createdNewUser = dataService.CreateUser(
+            newUser.Username, newUser.Email, newUser.Password, newUser.Salt
+        );
+        Assert.NotNull(dataService.GetUser(createdNewUser.Id));
+        dataService.DeleteUser(createdNewUser.Id);
+    }
+
+    [Fact]
+    public void UpdateUser_NewUser_UpdatesNewUser()
+    {
+        var dataService = new UserDataService();
+        var newUser = new
+        {
+            Username = "new_user",
+            Email = "new_user@email.com",
+            Password = "1234",
+            Salt = "12341234"
+        };
+        var createdNewUser = dataService.CreateUser(
+            newUser.Username, newUser.Email, newUser.Password, newUser.Salt
+        );
+        var updatedUser = dataService.UpdateUser(
+            createdNewUser.Id,
+            "updated_username",
+            "updated@email.com",
+            createdNewUser.Password,
+            createdNewUser.Salt,
+            createdNewUser.Role
+        );
+        Assert.Equal("updated_username", dataService.GetUser(createdNewUser.Id)!.Username);
+        dataService.DeleteUser(createdNewUser.Id);
+    }
+
+    [Fact]
+    public void DeleteUser_NewUser_DeletesNewUser()
+    {
+        var dataService = new UserDataService();
+        var newUser = new
+        {
+            Username = "new_user",
+            Email = "new_user@email.com",
+            Password = "1234",
+            Salt = "12341234"
+        };
+        var createdNewUser = dataService.CreateUser(
+            newUser.Username, newUser.Email, newUser.Password, newUser.Salt
+        );
+        Assert.NotNull(dataService.GetUser(createdNewUser.Id));
+        dataService.DeleteUser(createdNewUser.Id);
+        Assert.Null(dataService.GetUser(createdNewUser.Id));
     }
 }
