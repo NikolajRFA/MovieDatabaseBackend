b1dc5762cbf55ed0924ff18f9d1b68a7fae43a89
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2023-12-11 23:09:28 +0100

    Created endpoint for getting titles from person

--- WebServer/Controllers/PersonsController.cs
+++ WebServer/Controllers/PersonsController.cs
@@ -40,11 +40,38 @@ public class PersonsController : GenericControllerBase
         return Ok(Paging(dtos, total, new PagingValues { Page = page, PageSize = pageSize }, nameof(GetPersons)));
     }
 
+    [HttpGet("{nconst}/titles", Name = nameof(GetTitlesFromPerson))]
+    public IActionResult GetTitlesFromPerson(string nconst, int page = 0, int pageSize = 10)
+    {
+        var (titles, total) = _dataService.GetTitlesFromPerson(nconst, page, pageSize);
+        List<TitleDto> dtos = new();
+        foreach (var title in titles)
+        {
+            var dto = Mapper.Map<TitleDto>(title);
+            dto.Episodes = title.TitleType.Equals("tvSeries")
+                ? GetUrl(nameof(TitlesController.GetEpisodesOfSeries), new { tconst = title.Tconst.Trim() })
+                : null;
+            dto.Aliases = GetUrl(nameof(TitlesController.GetTitleAliases), new { tconst = title.Tconst.Trim() });
+            dto.Url = GetUrl(nameof(TitlesController.GetTitle), new { tconst = title.Tconst.Trim() });
+            dto.Genres = Mapper.Map<List<GenreDto>>(title.Genre);
+            dto.Crew = GetUrl(nameof(CrewController.GetCrew), new { tconst = title.Tconst.Trim() });
+            dtos.Add(dto);
+        }
+
+        return Ok(Paging(dtos, total, new NconstPagingValues {Nconst = nconst, Page = page, PageSize = pageSize },
+            nameof(GetTitlesFromPerson)));
+    }
+
     private PersonDto MapPerson(Person person)
     {
         var dto = Mapper.Map<PersonDto>(person);
         dto.Url = GetUrl(nameof(GetPerson), new { nconst = person.Nconst.Trim() });
         dto.Professions = Mapper.Map<List<ProfessionDto>>(person.Professions);
         return dto;
+    }
+    
+    private class NconstPagingValues : PagingValues
+    {
+        public string Nconst { get; set; }
     }
 }
